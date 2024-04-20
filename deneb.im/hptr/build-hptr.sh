a5b36d9517b7f1d53195d150cc35fa838b34a0b7
Peter Hajdu <peter@hajdu.im>
2024-04-20 15:13:07 +0200

    add utf-8 charset for hptr@deneb

--- deneb.im/hptr/build-hptr.sh
+++ deneb.im/hptr/build-hptr.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
 echo "<html>"
+echo "<head>"
+echo "<title>hptr@deneb.im / posts</title>"
+echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'
+echo "</head>"
 echo "<h1 style="text-align:center">hptr@deneb.im</h1>"
 lowdown public_gopher/*.md
 echo "</html>"
