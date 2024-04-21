6d929e14a44d84ac6cd8bbba0d25a523e9ba74f8
Peter Hajdu <peter@hajdu.im>
2024-04-21 11:49:42 +0200

    add style to deneb/~hptr

--- deneb.im/hptr/build-hptr.sh
+++ deneb.im/hptr/build-hptr.sh
@@ -1,32 +1,55 @@
 #!/usr/bin/env sh
 
-function build_index {
+function build_header {
   echo "<html>"
   echo "<head>"
-  echo "<title>hptr@deneb.im</title>"
+  echo "<title>hptr</title>"
+  echo "<style>"
+  echo "  @media (prefers-color-scheme: dark){"
+  echo "    body {"
+  echo "      background:#000"
+  echo "    }"
+  echo "  }"
+  echo "  body{"
+  echo "    margin:1em auto;"
+  echo "    max-width:40em;"
+  echo "    padding:0 .62em 3.24em;"
+  echo "    font:1.2em/1.62 sans-serif"
+  echo "  }"
+  echo "  h1,h2,h3 {"
+  echo "    line-height:1.2"
+  echo "  }"
+  echo "  @media print{"
+  echo "    body{"
+  echo "      max-width:none"
+  echo "    }"
+  echo "  }"
+  echo "</style>"
   echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'
   echo "</head>"
-  echo "<body style=""text-align:center"">"
-  echo "<h1>hptr@deneb.im</h1>"
-  for file in `ls -1 public_gopher/posts/`; do
-    echo "<a href=""posts/$file.html"">$file</a><br>"
-  done
+  echo "<body>"
+  echo "<h1>hptr</h1>"
+  echo "contact: hptr@deneb.im<br>"
+}
+
+function build_footer {
   echo "</body>"
   echo "</html>"
 }
 
+function build_index {
+  build_header
+  for file in `ls -1 public_gopher/posts/`; do
+    echo "<a href=""posts/$file.html"">$file</a><br>"
+  done
+  build_footer
+}
+
 function build_post {
   md_file=$1
-  echo "<html>"
-  echo "<head>"
-  echo "<title>hptr@deneb.im</title>"
-  echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'
-  echo "</head>"
-  echo "<body>"
-  echo "<h1 style=""text-align:center"">hptr@deneb.im</h1>"
+  build_header
   lowdown public_gopher/posts/$md_file
-  echo "</body>"
-  echo "</html>"
+  build_footer
 }
 
 build_index > public_html/index.html
