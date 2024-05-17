c497d418aa20aa8aba839ace82ddf79f7658c31b
Peter Hajdu <peter@hajdu.im>
2024-05-17 21:42:39 +0200

    add rss generation for gemini capsule

--- deneb.im/hptr/build-gemini.sh
+++ deneb.im/hptr/build-gemini.sh
@@ -13,9 +13,31 @@ function build_index {
   done
 }
 
+function build_rss {
+  echo '<?xml version="1.0" encoding="UTF-8" ?>'
+  echo '<rss version="2.0">'
+  echo '<channel>'
+  echo "<title>hptr's gemini capsule</title>"
+  echo "<description>hptr's rambling about life, the universe, and everything</description>"
+  echo '<link>gemini://deneb.im/users/hptr/</link>'
+
+  for file in `ls -t ./posts`; do
+    echo '<item>'
+    echo "<title>$file</title>"
+    echo "<description>$file</description>"
+    echo "<link>gemini://deneb.im/users/hptr/posts/$file</link>"
+    echo "<guid>gemini://deneb.im/users/hptr/posts/$file</guid>"
+    echo '</item>'
+  done
+
+  echo '</channel>'
+  echo '</rss>'
+}
+
 build_index > gemini/index.gmi
 mkdir -p gemini/posts
 for post in `ls -t ./posts`; do
   ~/go/bin/md2gmi -i posts/$post -o gemini/posts/$post
 done
 
+build_rss > gemini/rss.xml
